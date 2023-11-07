da5df494acd599626a10d600bc08b8f13eda3b3f
Towhidul123 <excaliburfsh@gmail.com>
2023-11-07 23:40:53 +0600

    added delete section

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://127.0.0.1:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -34,10 +39,20 @@ async function run() {
 
     //auth related api
     app.post('/jwt',async(req,res)=>{
+    
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user,'secret', {expiresIn: '1h'})
-      res.send(token);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+
+
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+      
+      })
+      .send({success: true});
+    
     })
 
     //services related api
@@ -112,19 +127,22 @@ async function run() {
 
     //updating
 
-    app.patch('/addToCart/:id', async(req,res)=>{
-      const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
-      const updatedBooking = req.body;
-      console.log(updatedBooking);
-      const updateDoc ={
-        $set: {
-          status: updatedBooking.status
-        },
-      };
-      const result = await userCollection.updateOne(filter,updateDoc);
-      res.send(result);
-    })
+    // Add a new route for updating the booking date
+app.patch('/updateBookingDate/:id', async(req,res)=>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const updatedBooking = req.body;
+  const newDate = updatedBooking.newDate; // Assuming you're sending the new date in the request body
+
+  const updateDoc ={
+    $set: {
+      formattedDate: newDate // Update the property name accordingly
+    },
+  };
+  const result = await userCollection.updateOne(filter, updateDoc);
+  res.send(result);
+})
+
 
 
     // Send a ping to confirm a successful connection
